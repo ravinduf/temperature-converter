6c15ccc810e5f22a7e02ba3ff613df87416c0bbb
ravinduf <ravinduf@rocketmail.com>
2020-04-15 13:40:50 +0530

    cached the elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
